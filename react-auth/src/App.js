fb012b2e59aadc7b1559f82135e7131d7a0f9390
Kristovel Edoway <kristovele@gmail.com>
2022-07-10 16:42:56 +0900

    Done_LaravelReactJs
    Selesai untuk Register, Login dan Logout.

---
+++ react-auth/src/App.js
@@ -0,0 +1,28 @@
+//import react
+import React from 'react';
+
+//import react router dom
+import { Switch, Route } from "react-router-dom";
+
+//import component Register
+import Register from './pages/Register';
+
+//import component Login
+import Login from './pages/Login';
+
+//import component Register
+import Dashboard from './pages/Dashboard';
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/dashboard" component={Dashboard} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
